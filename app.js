d3fc31bbf0fca1ff4ef50c28dd71eed86bbd3fc5
cosminadudas <cosmina.dudas@yahoo.ro>
2022-01-16 18:39:47 +0200

    added a new variable in order to get the max allowed chars value dynamically depending on the maxLength property of input element

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let inputElement = document.querySelector('input');
+let remainingCharsElement = document.getElementById('remaining-chars');
 
+let maxAllowedChars = inputElement.maxLength;
 
 function getAllowedNumberOfCharacters(event) {
     let insertedValue = event.target.value;
-
     let insertedValueLength = insertedValue.length;
-    let remainingCharsElement = document.getElementById('remaining-chars');
-    let remainingCharsAllowed = 60 - insertedValueLength;
+
+    let remainingCharsAllowed = maxAllowedChars - insertedValueLength;
     remainingCharsElement.innerHTML = remainingCharsAllowed;
     if (remainingCharsAllowed <= 10) {
         remainingCharsElement.classList.add("chars-limit-warning");
